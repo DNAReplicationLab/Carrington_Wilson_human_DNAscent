b9cd608e2cd24e2c6726b41c028b17dc61738846
RHCWilson <rhcwilson@gmail.com>
2020-11-05 21:32:50 +0000

    Feat: in process add paths

--- bwa_map.bash
+++ bwa_map.bash
@@ -8,35 +8,48 @@
 #SBATCH --output=/data/nieduszynski/NGS/2020_07_08_JC_RW_ILL_HeLa_BrdU_lowchases/bwa/bwa_out.%j
 #SBATCH --error=/data/nieduszynski/NGS/2020_07_08_JC_RW_ILL_HeLa_BrdU_lowchases/bwa/bwa_err.%j
 
-# usage sbatch bwa_map.bash <samples.txt> NB <samples.txt> should have stem of the filename with no extension
+# usage sbatch bwa_map.bash -f </path/to/folder/with/fastq/files> -l </path/to/samples.txt> 
+
+# NB <samples.txt> should have stem of the filename with no extension
+# For use on barcode trimmed fastq files in trim/ directory wihin run directory, run script in bwa/ directory under run directory
 
 # set environment variable for location of bowtie indexes on the server
 #export BOWTIE2_INDEXES="/data/nieduszynski/RESOURCES/REFERENCE_GENOMES"
 # load required modules and set variables
 module load SLURM/5.08.6
 module load BWA/0.7.12 && BWA="bwa"
+# requires SAMTOOLS/1.10 to use -M flag for samtools view. To use with older versions, remove -M flag from samtools view command,
+# and for samtools markdups command replace -f flag (and associated filename to save stats to) with -s, stats will be printed to screen.
 module unload SAMTOOLS
 module load SAMTOOLS/1.10 && SAMTOOLS="samtools"
-module load PICARDTOOLS/6.15 && PICARDTOOLS="/software/BIOINFORMATICS/PICARDTOOLS/picard-06.15/dist/picard.jar"
+#module load PICARDTOOLS/6.15 && PICARDTOOLS="/software/BIOINFORMATICS/PICARDTOOLS/picard-06.15/dist/picard.jar"
 module load BEDTOOLS/2.26.0 && BEDTOOLS="bedtools"
 echo "Required modules are loaded"
 
-# requires SAMTOOLS/1.10 to use -M flag for samtools view. To use with older versions, remove -M flag from samtools view command, and for samtools markdups command replace -f flag (and associated filename to save stats to) with -s, stats will be printed to screen.
+# collect command line varibles
+while [ "$1" != "" ]; do
+        case $1 in
+                -f )    shift
+                        FASTQ="$1" ;;
+                -l )    shift
+                        SAMPLES="$1" ;;
+        esac
+                shift
+done
 
-#For use on barcode trimmed file in trim/ directory wihin run directory, run script in bwa/ directory under run directory
 
-for file in $(cat $1)
+for SAMPLE in $(cat "$SAMPLES")
 do
-echo $file
+echo "$SAMPLE"
 
 #map trimmed reads to specified human reference genome (no alt version) using bwa-mem, sort and save as a bam file
-"$BWA" mem -M -t 16 /data/nieduszynski/RESOURCES/REFERENCE_GENOMES/STAR/ncbi-noALT-2020_03_17/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna ../trim/$file.fastq.gz | "$SAMTOOLS" sort -@ 15 -o $file.bwa.sorted.bam -
-echo $file.bwa.sorted.bam saved
-echo ""
+"$BWA" mem -M -t 16 /data/nieduszynski/RESOURCES/REFERENCE_GENOMES/STAR/ncbi-noALT-2020_03_17/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna "$FASTQ""$SAMPLE".fastq.gz | "$SAMTOOLS" sort -@ 15 -o "$SAMPLE".bwa.sorted.bam -
+echo "$SAMPLE".bwa.sorted.bam saved
+echo
 
 #index the bam file, print mapping stats to <sample>.mappingStats.txt file
-"$SAMTOOLS" index $file.bwa.sorted.bam
-touch $file.mappingStats.txt
+"$SAMTOOLS" index "$SAMPLE".bwa.sorted.bam
+touch "$SAMPLE".mappingStats.txt
 "$SAMTOOLS" flagstat $file.bwa.sorted.bam > $file.mappingStats.txt
 echo "uniquely mapped reads " >> $file.mappingStats.txt
 "$SAMTOOLS" view -h -@ 15 -F 3844 -q 1 $file.bwa.sorted.bam | grep -v -E "SA:Z:|XA:Z:" | wc -l >> $file.mappingStats.txt
