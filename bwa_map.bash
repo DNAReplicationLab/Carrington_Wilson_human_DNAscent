ce500d22e9dcc7469199dec20bc634437068a94b
RHCWilson <rhcwilson@gmail.com>
2020-11-24 13:34:57 +0000

    INIT: files for processing IP data

--- bwa_map.bash
+++ bwa_map.bash
@@ -5,16 +5,14 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=1
 #SBATCH --time=7-00:00:00
-#SBATCH --output=/data/nieduszynski/NGS/2020_07_08_JC_RW_ILL_HeLa_BrdU_lowchases/bwa/bwa_out.%j
-#SBATCH --error=/data/nieduszynski/NGS/2020_07_08_JC_RW_ILL_HeLa_BrdU_lowchases/bwa/bwa_err.%j
+#SBATCH --output="$SAVEPATH"bwa_map_out.%j
+#SBATCH --error="$SAVEPATH"bwa_map_err.%j
 
-# usage sbatch bwa_map.bash -f </path/to/folder/with/fastq/files> -l </path/to/samples.txt> 
+# usage sbatch bwa_map.bash -f </path/to/folder/with/fastq/files> -s </path/to/samples.txt> -o </path/to/save/files/> -g </path/to/reference/genome> -L </path/to/bed/of/of/genome/region/to/include>
 
 # NB <samples.txt> should have stem of the filename with no extension
-# For use on barcode trimmed fastq files in trim/ directory wihin run directory, run script in bwa/ directory under run directory
+# For use on barcode trimmed fastq.gz files
 
-# set environment variable for location of bowtie indexes on the server
-#export BOWTIE2_INDEXES="/data/nieduszynski/RESOURCES/REFERENCE_GENOMES"
 # load required modules and set variables
 module load SLURM/5.08.6
 module load BWA/0.7.12 && BWA="bwa"
@@ -31,30 +29,39 @@ while [ "$1" != "" ]; do
         case $1 in
                 -f )    shift
                         FASTQ="$1" ;;
-                -l )    shift
+                -s )    shift
                         SAMPLES="$1" ;;
+		-o )	shift
+			SAVEPATH="$1" ;;
+		-g )	shift
+			GENOME="$1" ;;
+		-L )	shift
+			INCLUDE="$1" ;;
         esac
                 shift
 done
 
+echo "Map reads for samples in $SAMPLES FASTQ files in $FASTQ and save at $SAVEPATH. Use $GENOME reference genome and $WINDOWS."
 
 for SAMPLE in $(cat "$SAMPLES")
 do
 echo "$SAMPLE"
+echo
 
 #map trimmed reads to specified human reference genome (no alt version) using bwa-mem, sort and save as a bam file
-"$BWA" mem -M -t 16 /data/nieduszynski/RESOURCES/REFERENCE_GENOMES/STAR/ncbi-noALT-2020_03_17/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna "$FASTQ""$SAMPLE".fastq.gz | "$SAMTOOLS" sort -@ 15 -o "$SAMPLE".bwa.sorted.bam -
+echo "$BWA" mem -M -t 16 "$GENOME" "$FASTQ""$SAMPLE".fastq.gz | "$SAMTOOLS" sort -@ 15 -o "$SAVEPATH""$SAMPLE".bwa.sorted.bam -
 echo "$SAMPLE".bwa.sorted.bam saved
 echo
 
-#index the bam file, print mapping stats to <sample>.mappingStats.txt file
-"$SAMTOOLS" index "$SAMPLE".bwa.sorted.bam
-touch "$SAMPLE".mappingStats.txt
-"$SAMTOOLS" flagstat $file.bwa.sorted.bam > $file.mappingStats.txt
-echo "uniquely mapped reads " >> $file.mappingStats.txt
-"$SAMTOOLS" view -h -@ 15 -F 3844 -q 1 $file.bwa.sorted.bam | grep -v -E "SA:Z:|XA:Z:" | wc -l >> $file.mappingStats.txt
+#index the bam file, print mapping stats to <$SAMPLE>.mappingStats.txt file
+echo "$SAMTOOLS" index "$SAVEPATH""$SAMPLE".bwa.sorted.bam
+touch "SAVEPATH""$SAMPLE".mappingStats.txt
+echo "$SAMTOOLS" flagstat "$SAVEPATH""$SAMPLE".bwa.sorted.bam > "$SAVEPATH""$SAMPLE".mappingStats.txt
+echo "uniquely mapped reads " >> "$SAVEPATH""$SAMPLE".mappingStats.txt
+"$SAMTOOLS" view -h -@ 15 -F 3844 -q 1 "$SAVEPATH""$SAMPLE".bwa.sorted.bam | grep -v -E "SA:Z:|XA:Z:" | wc -l >> "$SAVEPATH""$SAMPLE".mappingStats.txt
 
 #select uniquely mapping reads, mark and remove duplicaes (duplicate stats saved in <sample>.markdups.txt file, generate genome coverage for the 5' end of reads, keep only bases with >= 1 read mapping there and save as a bed file.
-"$SAMTOOLS" view -h -@ 15 -F 3844 -q 1 -M -L /data/nieduszynski/NGS/2020_03_04_ILL_RW_HeLa_Chases/genomeWindows/hg38_10kbWindows.bed $file.bwa.sorted.bam | grep -v -E "SA:Z:|XA:Z:" | "$SAMTOOLS" view -@ 15 -b - | "$SAMTOOLS" markdup -r -f $file.markdups.txt - - | "$BEDTOOLS" genomecov -5 -d -ibam stdin | awk 'BEGIN {OFS="\t"} {if ($3>0) print $1,$2,$2,"name",$3}' > $file.coverage.bed
-echo $file.coverage.bed saved
+echo "$SAMTOOLS" view -h -@ 15 -F 3844 -q 1 -M -L "$INCLUDE" "$SAVEPATH""$SAMPLE".bwa.sorted.bam | grep -v -E "SA:Z:|XA:Z:" | "$SAMTOOLS" view -@ 15 -b - | "$SAMTOOLS" markdup -r -f "$SAVEPATH""$SAMPLE".markdups.txt - - | "$BEDTOOLS" genomecov -5 -d -ibam stdin | awk 'BEGIN {OFS="\t"} {if ($3>0) print $1,$2,$2,"name",$3}' > "$SAVEPATH""$SAMPLE".coverage.bed
+echo "$SAMPLE".coverage.bed saved
+echo
 done
---
+++ gencoverageToBigWig.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Generates intermediate BigWig files for coverage of IP or INPUT files for visualisation and generates intermediate file for performing IPoverINPUT step, both from genomecoverage bed files generated from bwa_map.bash..
+
+# usage bash gencoverageToBigWig.bash -o </path/to/folder/with/files/and/for/save> -s </path/to/my_samples.txt> -b </path/to/blacklist> -w </path/to/windows/bed/file> -n <window_name> -c </path/to/chromosome/size/bed/file>
+
+# NB my_samples.txt should have stem of the filename with no extension
+# do not use with sbatch - if need to,  need to change sort -k1,1 -k2,2n to samtools sort.
+
+# collect command line varibles
+while [ "$1" != "" ]; do
+        case $1 in
+                -o )    shift
+                        SAVEPATH="$1" ;;
+                -s )    shift
+                        SAMPLES="$1" ;;
+                -b )    shift
+                        BLACKLIST="$1" ;;
+                -w )    shift
+                        WINDOWS="$1" ;;
+                -n )    shift
+                        WINDOW="$1" ;;
+	        -c )	shift
+			CHROMSIZE="$1" ;;
+esac
+                shift
+done
+
+echo "Generate bigwig files for samples in $SAMPLES and save at $SAVEPATH. Remove $BLACKLIST and map into $WINDOW size windows using $WINDOWSand $CHROMSIZE."
+
+# Porcess files listed in $SAMPLES
+for SAMPLE in $(cat "$SAMPLES")
+	do
+	echo "$SAMPLE"
+	# remove any positions in blacklist, map into windows of chosen size, sort, awk command removes any empty windows
+	bedtools intersect -a "$SAVEPATH""$SAMPLE".coverage.bed -b "$BLACKLIST" -v | bedtools map -o sum -null 0 -a "$WINDOWS" -b stdin | sort -k1,1 -k2,2n | awk 'BEGIN {OFS="\t"} {if ($4>0) print $1,$2,$3,$4}' > "$SAVEPATH""$SAMPLE".sorted."$WINDOW".bed
+	# convert to bigwig files for viewing in IGV
+	bedGraphToBigWig "$SAVEPATH""$SAMPLE".sorted."$WINDOW".bed "$CHROMSIZE" "$SAVEPATH""$SAMPLE"."$WINDOW".bw
+	# Generate intermediate windowed bed file for all samples ready for IPoverINPUT step (making join_by column)
+        awk '{print $1, $2, $3, $4, $1"_"$2}' "$SAVEPATH""$SAMPLE".sorted."$WINDOW".bed | sort -k 5 >  "$SAVEPATH""$SAMPLE"."$WINDOW".forjoin
+	echo "$SAMPLE.$WINDOW.bw saved"
+	echo
+done
