fb499120d1aeb173af15340a51dd78eeab8cbdda
can1002 <can1002@yahoo.com>
2021-01-25 17:19:00 +0000

    Made info function and used this to replace most echo commands

--- runDNAscent.bash
+++ runDNAscent.bash
@@ -227,8 +227,8 @@ function basecall_fn() {
 
 	# check worked
 	if [[ -f "$RUNPATH"reads.fastq ]]; then
-		echo "$RUNPATH fastq files generated and tidied"
-		echo
+		info "$RUNPATH fastq files generated and tidied"
+		info
 		else
 		die "Exit, $RUNPATH reads.fastq not made, check guppy log files"
 	fi
@@ -247,8 +247,8 @@ function mapping_fn() {
 	samtools index "$RUNPATH"alignments.sorted
 
 	if [[ -f "$RUNPATH"alignments.sorted ]] ; then
-		echo "$FASTQ" reads mapped to reference.
-		echo
+		info "$FASTQ" reads mapped to reference.
+		info
 		else
 		die "Exit, $RUNPATH alignments.sorted not made, check minimap log files"
 	fi
@@ -279,23 +279,23 @@ function sub_bam_fn() {
 # NOTE: This still needs to be generalised and adapted for SLURM use
 function dnascent_fn() {
 	if [[ ! -f "$RUNPATH"index.dnascent ]]; then
-		echo "DNAscent index"
-		echo
+		info "DNAscent index"
+		info
 		DNAscent index -f "$FAST5" -s "$RUNPATH"sequencing_summary.txt -o "$RUNPATH"index.dnascent 2> "$RUNPATH""$SAVEDIR"/logfiles/index_output.txt
 	fi
 
-	echo
-	echo "DNAscent detect"
-	echo
+	info
+	info "DNAscent detect"
+	info
 	DNAscent detect -b "$BAM" -r "$REFGENOME" -i "$RUNPATH"index.dnascent -o "$RUNPATH""$SAVEDIR"/"$NAME".detect -t 50 --GPU 0 -l "$DETECTTHRESHOLD" 2> "$RUNPATH""$SAVEDIR"/logfiles/detect_output.txt
 
 	if [[ -f "$RUNPATH""$SAVEDIR"/"$NAME".detect ]] ; then
-		echo "$RUNPATH""$SAVEDIR" detect complete.
-		echo
+		info "$RUNPATH""$SAVEDIR" detect complete.
+		info
 		else
 		die "Exit, detect file not made, check detect log files"
 	fi
-	echo "make bedgraphs"
+	info "make bedgraphs"
 	python "$python_utils_dir"/dnascent2bedgraph.py -d "$RUNPATH""$SAVEDIR"/"$NAME".detect -o "$RUNPATH""$SAVEDIR"/"$NAME".bedgraphs 2> "$RUNPATH""$SAVEDIR"/logfiles/dnascent_bedgraph_output.txt
 }
 
@@ -305,20 +305,20 @@ function dnascent_fn() {
 # NOTE: This still needs to be generalised and adapted for SLURM use
 function forksense_fn() {
 	touch "$RUNPATH""$SAVEDIR"/logfiles/forkSense_output.txt
-	echo "DNAscent forkSense"
-	echo
+	info "DNAscent forkSense"
+	info
 	DNAscent forkSense -d "$RUNPATH""$SAVEDIR"/"$NAME".detect -o "$RUNPATH""$SAVEDIR"/"$NAME".forkSense --markOrigins --markTerminations 2> "$RUNPATH""$SAVEDIR"/logfiles/forkSense_output.txt
 	# to fix forksense save location bug
 	mv "$RUNPATH"origins_DNAscent_forkSense.bed "$RUNPATH""$SAVEDIR"
 	mv "$RUNPATH"terminations_DNAscent_forkSense.bed "$RUNPATH""$SAVEDIR"
 
 	if [[ -f "$RUNPATH""$SAVEDIR"/"$NAME".forkSense ]]; then
-		echo "$RUNPATH""$SAVEDIR" forksense complete.
-		echo
+		info "$RUNPATH""$SAVEDIR" forksense complete.
+		info
 	else
 	die "Exit, forksense file not found, check forksense log file"
 	fi
-	echo "make bedgraphs"
+	info "make bedgraphs"
 	python "$python_utils_dir"/dnascent2bedgraph.py -f "$RUNPATH""$SAVEDIR"/"$NAME".forkSense -o "$RUNPATH""$SAVEDIR"/"$NAME".bedgraphs 2> "$RUNPATH"/"$SAVEDIR"/logfiles/forkSense_bedgraph_output.txt
 
 }
@@ -369,28 +369,37 @@ function script_init() {
     readonly guppy_model="dna_r9.4.1_450bps_fast.cfg"			# guppy model to use for basecalling
 }
 
+# DESC: Script to output information to screen (if verbose) or summary file
+# ARGS: Informative text to output
+# OUTS: 
+# NOTE:
+function info() {
+	if [[ "$verbose" == true ]]; then
+	    echo "INFO: $@" 
+	else
+		echo "INFO: $@" > "$RUNPATH""$SAVEDIR"/logfiles/summary.txt 
+}
+
 # DESC: Prints variables those variables that have been set, if verbose true
 # ARGS: None
 # OUTS: None
 # NOTE:
 function print_variables() {
-	if [[ "$verbose" == true ]]; then
-		echo Run script at $RUNSCRIPT
-		echo BASECALL and mapping = $BASECALL
-		echo MAPPING only = $MAPPING
-		echo FORKSENSE = $FORKSENSE
-		echo FAST5 = $FAST5
-		echo RUNPATH = $RUNPATH
-		echo SAVEDIR = $SAVEDIR
-		echo REFGENOME = $REFGENOME
-		echo DETECTTHRESHOLD = $DETECTTHRESHOLD
-		echo NAME = $NAME
-		echo REGION = $REGION
-		echo SUBSAMPLE = $SUBSAMPLE
-		echo "BAM to use for detect" = $BAM
- 		echo "Fastq file to use" = $FASTQ
-		echo
-	fi
+	info Run script at $RUNSCRIPT
+	info BASECALL and mapping = $BASECALL
+	info MAPPING only = $MAPPING
+	info FORKSENSE = $FORKSENSE
+	info FAST5 = $FAST5
+	info RUNPATH = $RUNPATH
+	info SAVEDIR = $SAVEDIR
+	info REFGENOME = $REFGENOME
+	info DETECTTHRESHOLD = $DETECTTHRESHOLD
+	info NAME = $NAME
+	info REGION = $REGION
+	info SUBSAMPLE = $SUBSAMPLE
+	info "BAM to use for detect" = $BAM
+	info "Fastq file to use" = $FASTQ
+	info
 }
 
 # DESC: Script initialisation for use on Nieduszynski server at UoO
@@ -474,8 +483,8 @@ if [[ "$FORKSENSE" == true ]]; then
 fi
 
 if [[ -d "$RUNPATH""$SAVEDIR"/"$NAME".bedgraphs ]]; then
-	echo
-	echo "$RUNPATH""$SAVEDIR" bedgraphs saved.
+	info
+	info "$RUNPATH""$SAVEDIR" bedgraphs saved.
 	else
 	die "Exit, $NAME.bedgrpahs folder not found. Check bedgraphs logfile"
 fi
