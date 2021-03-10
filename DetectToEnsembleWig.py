7e7934a94a323052c4ad6510015e4507e6c0056f
can1002 <can1002@yahoo.com>
2021-03-10 09:28:33 +0000

    Tested and debugged version; writes wig to file

--- DetectToEnsembleWig.py
+++ DetectToEnsembleWig.py
@@ -1,60 +1,75 @@
+#----------------------------------------------------------
+# Copyright 2020-2021 Earlham Institute
+# Written by Conrad Nieduszynski (conrad.nieduszynski@earlham.ac.uk)
+# This software is licensed under GPL-3.0.  You should have
+# received a copy of the license with this software.  If
+# not, please Email the author.
+#----------------------------------------------------------
+
+# Usage: python DetectToEnsembleWig.py path/to/input/detect/file window_in_kb path/for/output/wig/file
+# example command:
+# python DetectToEnsembleWig.py test.detect 100 test.wig
+
 import sys
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
 
-target = 'III'
-length = 2452883
-BrdUCalls = [0]*length
-coverage = [0]*length
-BrdUCalls = np.array(BrdUCalls)
-coverage = np.array(coverage)
+chromosomes = 22
+maxLength = 248956422 // 1000
+
+BrdUCalls = np.zeros(shape=(chromosomes,maxLength))
+coverage = np.zeros(shape=(chromosomes,maxLength))
+threshold = 0.5
+SmallWindow = 1000
+LargeWindow = int(sys.argv[2])
 
 #import detect data
 f = open(sys.argv[1],'r')
 for line in f:
 
-        if line[0] == '#':
-    
-            continue
-        
-        
-        if line[0] == '>':
+	if line[0] == '#':
+
+		continue
+	
+	
+	if line[0] == '>':
 
 		splitLine = line.rstrip().split(' ')
 		chromosome = splitLine[1]
-
+# 		print(chromosome)
 		continue
+
 
 	else:
 
-		splitLine = line.rstrip().split('\t')
+		if chromosome[3:].isnumeric():
+			MyChromosome = int(chromosome[3:])
+		
+			splitLine = line.rstrip().split('\t')
+			coverage[MyChromosome-1][int(splitLine[0])//SmallWindow] += 1
+			if float(splitLine[1]) > threshold:
 
-		if chromosome == target:
-
-			coverage[int(splitLine[0])] += 1
-			if float(splitLine[1]) > 0.12:
-
-				BrdUCalls[int(splitLine[0])] += 1
-
-
-
-xBrdU = []
-yBrdU = []
-for i in range( 0, length, 1000 ):
-
-	if float(sum( coverage[i:i+1000])) == 0.0:
-		continue
-	else:
-		yBrdU.append(float(sum( BrdUCalls[i:i+1000] )) / float(sum( coverage[i:i+1000])))
-		xBrdU.append(i+500)
-
-yBrdUSmooth = np.convolve(yBrdU, np.ones((10,))/10, mode='same')
+				BrdUCalls[MyChromosome-1][int(splitLine[0])//SmallWindow] += 1
 
 
-f = open('BrdU_data_chrIII.plot','w')
-f.write(str('variableStep chrom=chr3' + '\n'))
-for i, v in enumerate(yBrdUSmooth):
-    f.write(str(xBrdU[i]) + '\t' + str(v) + '\n')
-f.close()
+#export the ensemble data as a wig file
+ensembleBrdU = np.zeros(shape=(chromosomes,maxLength))
+
+f = open(sys.argv[3],'w')
+for chromo in range(len(ensembleBrdU)):
+	f.write('variableStep chrom=chr{}'.format(chromo+1) + '\n')
+	for i in range( 0, maxLength, LargeWindow ):
+
+		if float(sum( coverage[chromo][i:i+LargeWindow])) == 0.0:
+			continue
+		else:
+			ensembleBrdU[chromo][i + LargeWindow//2] = float(sum( BrdUCalls[chromo][i:i+1000] )) / float(sum( coverage[chromo][i:i+1000]))
+			f.write(str((i + LargeWindow//2)*1000) + '\t' + str("%.3f" % ensembleBrdU[chromo][i + LargeWindow//2]) + '\n')
+
+# yBrdUSmooth = np.convolve(yBrdU, np.ones((10,))/10, mode='same')
+# 
+# 
+# f = open('BrdU_data_chrIII.plot','w')
+# f.write(str('variableStep chrom=chr3' + '\n'))
+# for i, v in enumerate(yBrdUSmooth):
+#     f.write(str(xBrdU[i]) + '\t' + str(v) + '\n')
+# f.close()
